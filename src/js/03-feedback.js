b45c1296f8dc204980441ade4f6a4bd31397ef07
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-04 13:40:31 +0300

    Form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
 
+import throttle from 'lodash.throttle';
+
+const refs = {
+   form: document.querySelector('.feedback-form'),
+   email: document.querySelector('.feedback-form input'),
+   message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onForm, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+const formContainer = {};
+
+function onForm(e) {
+  formContainer[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formContainer));
+}
+
+function onSubmit(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (data) {
+   refs.email.value = data.email;
+   refs.message.value = data.message;
+  }
+})();
