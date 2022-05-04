9f6443099ff425da73f741544d730fb4cca3a3aa
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-04 11:06:32 +0300

    Video end picters

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 console.log(galleryItems);
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import templateFunction from '../templates/item-markup.hbs';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+(function createMarkup() {
+  const itemMarkup = galleryItems.map(templateFunction).join('');
+  galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
+})();
+
+
+
+new SimpleLightbox('.gallery a', {
+   captions: true,
+   captionsData: 'alt',
+   captionDelay: 250,
+ });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+import throttle from 'lodash.throttle';
+
+const iframePlayer = document.querySelector('iframe');
+const player = new Vimeo.Player(iframePlayer);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
