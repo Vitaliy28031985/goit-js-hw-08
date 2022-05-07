e354c7ac54b1321b3754ab66a31594094160422b
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-07 19:45:00 +0300

    19/44

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,8 +8,12 @@ const player = new Vimeo.Player(iframePlayer);
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
+  
   localStorage.setItem('videoplayer-current-time', seconds);
+
 }
 if (localStorage.getItem('videoplayer-current-time')) {
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-} 
+} else {
+  0;
+}
