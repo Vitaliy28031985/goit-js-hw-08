a7f4ef389e0eeca4d87012f515ed496e202be6d4
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-07 20:25:20 +0300

    20/25

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ import throttle from 'lodash.throttle';
 const iframePlayer = document.querySelector('iframe');
 const player = new Vimeo.Player(iframePlayer);
 
-player.on('timeupdate', throttle(onPlay, 1000));
+
 
 function onPlay({ seconds }) {
   
@@ -14,6 +14,6 @@ function onPlay({ seconds }) {
 }
 if (localStorage.getItem('videoplayer-current-time')) {
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-} else {
-  0;
 }
+
+player.on('timeupdate', throttle(onPlay, 1000));
