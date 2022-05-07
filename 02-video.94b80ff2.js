f8b134d84d5328dfe85ba4ee42edf8aec2c13bd0
Vitaliy28031985 <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-07 16:46:02 +0000

    Deploying to gh-pages from @ Vitaliy28031985/goit-js-hw-08@e354c7ac54b1321b3754ab66a31594094160422b 🚀

--- 02-video.94b80ff2.js
+++ 02-video.94b80ff2.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const r=document.querySelector("iframe"),o=new Vimeo.Player(r);function a({seconds:e}){localStorage.setItem("videoplayer-current-time",e)}o.on("timeupdate",(0,e.default)(a,1e3)),localStorage.getItem("videoplayer-current-time")&&o.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.61ed89d5.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.94b80ff2.js.map
